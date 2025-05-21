47d60fc693698cffef64ab05c892df46c502623e
KamilSab <kamilsab46@gmail.com>
2025-05-21 22:38:22 +0300

    hw9

--- app/src/main/java/ru/itis/androidlab2sem/presentation/main/MainState.kt
+++ app/src/main/java/ru/itis/androidlab2sem/presentation/main/MainState.kt
@@ -1,12 +1,19 @@
 package ru.itis.androidlab2sem.presentation.main
 
 import android.graphics.Bitmap
+import androidx.compose.runtime.Immutable
 
+@Immutable
 sealed class MainState {
+    @Immutable
     object Loading : MainState()
+
+    @Immutable
     data class Success(
         val bitmap: Bitmap,
         val source: String
     ) : MainState()
+
+    @Immutable
     data class Error(val message: String) : MainState()
 }
